566c6a1c9d1e21e4284efffe864aec0e21dac9b4
quinnh4 <qhasselgren@luc.edu>
2025-09-22 11:15:55 +0000

    fixed issue with scrolling

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -4,7 +4,7 @@
 #define MULTIBOOT2_HEADER_MAGIC 0xe85250d6
 
 const unsigned int multiboot_header[] __attribute__((section(".multiboot"))) =
-    {MULTIBOOT2_HEADER_MAGIC, 0, 16, -(16 + MULTIBOOT2_HEADER_MAGIC), 0, 12};
+    {MULTIBOOT2_HEADER_MAGIC, 0, 16, -(16+MULTIBOOT2_HEADER_MAGIC), 0, 12};
 
 #define VGA_ADDRESS 0xB8000
 #define VGA_COLS 80
@@ -15,10 +15,12 @@ struct termbuf {
     char color;
 };
 
-static int cursor_row = 0;
-static int cursor_col = 0;
+static struct termbuf *vram = (struct termbuf*)VGA_ADDRESS;
 
-static struct termbuf* const vram = (struct termbuf*)VGA_ADDRESS;
+// col
+int x = 0;
+// row
+int y = 0; 
 
 void scroll() {
     for (int row = 1; row < VGA_ROWS; row++) {
@@ -26,42 +28,42 @@ void scroll() {
             vram[(row - 1) * VGA_COLS + col] = vram[row * VGA_COLS + col];
         }
     }
-
     for (int col = 0; col < VGA_COLS; col++) {
         vram[(VGA_ROWS - 1) * VGA_COLS + col].ascii = ' ';
         vram[(VGA_ROWS - 1) * VGA_COLS + col].color = 7;
     }
 }
 
-int putc(int data) {
-    if (data == '\n') {
-        cursor_row++;
-        cursor_col = 0;
+int putc(int c) {
+    if (c == '\n') {
+        x = 0;
+        y++;
     } else {
-        vram[cursor_row * VGA_COLS + cursor_col].ascii = (char)data;
-        vram[cursor_row * VGA_COLS + cursor_col].color = 7;
-        cursor_col++;
-        if (cursor_col >= VGA_COLS) {
-            cursor_col = 0;
-            cursor_row++;
+        vram[y * VGA_COLS + x].ascii = (char)c;
+        vram[y * VGA_COLS + x].color = 7;
+        x++;
+        if (x >= VGA_COLS) {
+            x = 0;
+            y++;
         }
     }
 
-    if (cursor_row >= VGA_ROWS) {
+    if (y >= VGA_ROWS) {
         scroll();
-        cursor_row = VGA_ROWS - 1;
+        y = VGA_ROWS - 1;
     }
 
     return 0;
 }
 
-
 void main() {
-    esp_printf(putc, "Hello World!\n");
-    esp_printf(putc, "Execution level: %d\n", 0);
-
+    // to show scrolling pushes this off screen
+    esp_printf(putc, "Hello World!\r\n");
+    esp_printf(putc, "Execution level: %d\n");
+   
+    // set the loop to go to 23 so you can see the execution level
     for (int i = 0; i < 23; i++) {
-    	esp_printf(putc, "Scrolling at %d\n", i);
+         esp_printf(putc, "Scrolling\n");
     }
 
     while (1);
